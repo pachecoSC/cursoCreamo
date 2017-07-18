4094acd36c2069eec502edd88606e7af38ee42d2
cristophermorep <cris_tito_21@hotmail.com>
2017-07-18 11:35:07 -0500

    aplicacion

--- application/controllers/controlador_index.php
+++ application/controllers/controlador_index.php
@@ -4,12 +4,15 @@
 class Controlador_index extends CI_Controller{
     public function __construct() {
         parent::__construct();
-        
+        $this->load->model('Modelo_categoria');
     }
     //se llama a la vista
     public function index(){
         //$this->load->helper('url');
-        $this->load->view('principal');
+        $datos['categorias']= $this->Modelo_categoria->listarCategoria();
+        $data['contenido']=$this->load->view('index/index',$datos);
+        
+        $this->load->view('plantilla',$data);
          
     }
     
